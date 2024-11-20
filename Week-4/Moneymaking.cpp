325caf168e63e2b351a39b3a26403b6c8e56c46f
Imtiaz Deepto <74072999+imtiazdeepto@users.noreply.github.com>
2024-11-21 00:02:41 +0600

    20 Nov 2024

---
+++ Week-4/Moneymaking.cpp
@@ -0,0 +1,22 @@
+//https://www.codechef.com/problems/NUGGET
+//Solved at:20 nov 2024 8:35 pm
+#include<bits/stdc++.h>
+#define Code ios_base::sync_with_stdio(false);
+#define By cin.tie(NULL);
+#define ImtiazDeepto cout.tie(NULL);
+using namespace std;
+#define all(x)  (x).begin(), (x).end()
+#define int long long
+#define tc int x; cin >> x; while (x--)
+const int N=1e8;
+
+signed main(){
+
+Code By ImtiazDeepto
+int n,k;
+	cin>>n>>k;
+	cout<<(n*5000)+(k*9800)<<endl;
+
+
+return 0;
+}
