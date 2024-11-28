7319f1a29d2504f0a76ddcec9af7491edac8d296
Imtiaz Deepto <74072999+imtiazdeepto@users.noreply.github.com>
2024-11-28 23:53:43 +0600

    28/NOV/2024

---
+++ Week-5/Day-5/OR_Permutation.cpp
@@ -0,0 +1,33 @@
+//
+// Solved at:
+#include <bits/stdc++.h>
+#define Code ios_base::sync_with_stdio(false);
+#define By cin.tie(NULL);
+#define ImtiazDeepto cout.tie(NULL);
+using namespace std;
+#define all(x) (x).begin(), (x).end()
+#define int long long
+#define tc    \
+    int x;    \
+    cin >> x; \
+    while (x--)
+const int N = 1e8;
+
+signed main()
+{
+
+    Code By ImtiazDeepto
+
+        tc
+    {
+        int n;
+        cin >> n;
+        for (int i = n; i > 0; i--)
+        {
+            cout << i << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
