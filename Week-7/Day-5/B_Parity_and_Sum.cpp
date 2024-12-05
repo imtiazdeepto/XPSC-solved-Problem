0fd3a748082f502728c77e6fab3f0f7495bfcc89
Imtiaz Deepto <74072999+imtiazdeepto@users.noreply.github.com>
2024-12-06 00:03:39 +0600

    5-Dec-2024

---
+++ Week-7/Day-5/B_Parity_and_Sum.cpp
@@ -0,0 +1,73 @@
+//https://codeforces.com/problemset/problem/1993/B
+// Solved at:Dec/06/2024 00:01UTC+6
+#include <bits/stdc++.h>
+#define Code ios_base::sync_with_stdio(false);
+#define By cin.tie(NULL);
+#define ImtiazDeepto cout.tie(NULL);
+using namespace std;
+#define all(x) (x).begin(), (x).end()
+#define int long long
+#define tc    \
+    int x;    \
+    cin >> x; \
+    while (x--)
+const int N = 1e8;
+
+signed main()
+{
+
+    Code By ImtiazDeepto
+
+        tc
+    {
+        int n;
+        cin >> n;
+        vector<int> evn, odd;
+        while (n--)
+        {
+            int x;
+            cin >> x;
+            if (x & 1)
+            {
+                odd.push_back(x);
+            }
+            else
+            {
+                evn.push_back(x);
+            }
+        }
+        if (evn.size() == 0 || odd.size() == 0)
+        {
+            cout << 0 << endl;
+            continue;
+        }
+        else
+        {
+
+            sort(all(evn));
+            sort(all(odd));
+            int mx = odd.back();
+            int n = evn.size();
+            int ans = 0;
+            int yes=0;
+            for (int i = 0; i < n; i++)
+            {
+                if (evn[i] > mx)
+                {
+                    cout << n + 1 << endl;
+                    yes=1;
+                    break;
+                }
+                else
+                {
+                    ans++;
+                    mx += evn[i];
+                }
+            }
+            if(yes==0)
+            cout << ans << endl;
+        }
+    }
+
+    return 0;
+}
