051e1656ecf14d634d48e08986ef26bb3d614a3a
Imtiaz Deepto <74072999+imtiazdeepto@users.noreply.github.com>
2025-01-07 23:37:35 +0600

    Tue 7 Jan 2024

---
+++ Week-11/Day-4/C_Maximum_Median.cpp
@@ -0,0 +1,59 @@
+//
+#include <bits/stdc++.h>
+#include <iostream>
+#define Code ios_base::sync_with_stdio(false);
+#define By cin.tie(NULL);
+#define ImtiazDeepto cout.tie(NULL);
+using namespace std;
+#define tc    \
+    int x;    \
+    cin >> x; \
+    while (x--)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define all(x) (x).begin(), (x).end()
+#define int long long
+signed main()
+{
+    Code By ImtiazDeepto
+
+        int n,
+        k;
+    cin >> n >> k;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    auto ok = [&](int median)
+    {
+        long long cnt = 0;
+        for (int i = (n / 2); i < n; i++)
+        {
+            cnt += (a[i] < median ? (median - a[i]) : 0);
+        }
+        return cnt <= k;
+    };
+
+    sort(all(a));
+
+    int l = 1, r = 2e9, mid, ans;
+    while (l <= r)
+    {
+        mid = l + (r - l) / 2;
+        if (ok(mid))
+        {
+            ans = mid;
+            l = mid + 1;
+        }
+        else
+        {
+            r = mid - 1;
+        }
+    }
+
+    cout << ans << '\n';
+
+    return 0;
+}
